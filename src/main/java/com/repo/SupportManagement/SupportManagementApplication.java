88ddb07ce623c42c45f3fa6d1070ad34a0d58bf7
venkat.prasath <venkat.prasath@zohocorp.com>
2025-04-28 23:25:26 +0530

    basic spring boot application files added

---
+++ src/main/java/com/repo/SupportManagement/SupportManagementApplication.java
@@ -0,0 +1,13 @@
+package com.repo.SupportManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repo/SupportManagement/SupportManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repo.SupportManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
